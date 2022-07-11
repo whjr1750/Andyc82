b2d854bc828b0aceb7d5f14ecadaf252dca766b6
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2022-07-11 17:05:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 var mouseEvent = "empty";
-var last_position_of_x,last_position_of_y;
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
+var last_position_of_x, last_position_of_y;
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 color = "black";
 width_of_line = 1;
-canvas.addEventListener("mousedown",my_mousedown);
-function my_mousedown(e){
-//color = document.getElementById("color").value;//
-//width_of_line = document.getElementById("width_of_the_line").value;//
-mouseEvent="mouseDown";
+canvas.addEventListener("mousedown", my_mousedown); 
+function my_mousedown(e) {
+    color = document.getElementById("color").value;//
+    width_of_line = document.getElementById("width_of_line").value;//
+    mouseEvent = "mouseDown";
 }
-canvas.addEventListener("mouseMove",my_mousemove);
-function my_mousemove(e){
-    current_position_of_mouse_x = e.clientX - canvas.offSetLeft;
-    current_position_of_mouse_y = e.clientY - canvas.offSetTop;
-    if(mouseEvent == "mouseDown"){
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e) {
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft; // s was capital in this line
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    if (mouseEvent == "mouseDown") { //D was small case in this line
 
         ctx.beginPath();
-        ctx.strokeStyle=color;
-        ctx.lineWidth=width_of_line;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
         console.log("last postion of x and y coordinates =");
         console.log("X = " + last_position_of_x + "Y = " + last_position_of_y);
         ctx.moveTo(last_position_of_x, last_position_of_y);
         console.log("current position of x and y coordinates = ")
-        console.log("X = " + current_position_of_x + "Y = " + current_position_of_y);
+        console.log("X = " + current_position_of_mouse_x + "Y = " + current_position_of_mouse_y); //mouse word was missing here
         ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y)
         ctx.stroke();
     }
